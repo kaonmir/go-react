9cc1f42bd52d33337e879e0ef0d74a97646ac825
kaonmir <thswpvm@cau.ac.kr>
2021-08-21 07:36:16 +0900

    jk

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import GoPage from "./components/GoPage";
 
 function App() {
-  const onClick = (a: number): void => {};
   return (
     <div className="App">
       <GoPage />
